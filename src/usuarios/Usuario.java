d0e4f72b393e85ddb628df276578bddabac385c0
Peterson William <petersondev.profissional@gmail.com>
2023-12-16 16:04:13 -0300

    Criando classe de Usuário

---
+++ src/usuarios/Usuario.java
@@ -0,0 +1,57 @@
+package usuarios;
+
+public class Usuario {
+    private String nome;
+    private int idade;
+    private String sexo;
+    private String cpf;
+    private String rg;
+
+    public Usuario(String nome, int idade, String sexo, String cpf, String rg) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.cpf = cpf;
+        this.rg = rg;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+}
