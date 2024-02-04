530d5a2de0ff022710abb64e089a374e0caaf037
Peterson William <petersondev.profissional@gmail.com>
2024-02-04 16:39:23 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -14,64 +14,65 @@ public class Main {
                 Menu.exibirMenuConta(contaLogada.getTitular().getNome());
                 int opcao = ValidarEntradas.retornarInt("Digite a operação que deseja realizar: ");
 
-                switch (opcao) {
+                if (opcao == 1) {
 
-                    case 1:
-                        float valor = ValidarEntradas.retornarFloat("Digite o valor que deseja sacar: ");
-                        try {
-                            contaLogada.sacarDinheiro(valor);
-                        } catch (RuntimeException error) {
-                            System.err.println(error);
-                        }
-                        break;
+                    float valor = ValidarEntradas.retornarFloat("Digite o valor que deseja sacar: ");
+                    try {
+                        contaLogada.sacarDinheiro(valor);
+                    } catch (RuntimeException error) {
+                        System.err.println(error);
+                    }
 
-                    case 2:
-                        valor = ValidarEntradas.retornarFloat("Digite o valor que você deseja depositar: ");
+                } else if (opcao == 2) {
 
-                        contaLogada.depositarDinheiro(valor);
-                        break;
+                    float valor = ValidarEntradas.retornarFloat("Digite o valor que você deseja depositar: ");
+                    contaLogada.depositarDinheiro(valor);
 
-                    case 3:
-                        int numeroContaRecebedor = ValidarEntradas.retornarInt("Digite o número da conta do recebedor: ");
-                        int numeroAgenciaRecebedor = ValidarEntradas.retornarInt("Digite o número da agência do recebedor: ");
+                } else if (opcao == 3) {
 
-                        valor = ValidarEntradas.retornarFloat("Digite o valor da transferência: ");
-                        Conta contaRecebedor = banco.buscarConta(numeroContaRecebedor, numeroAgenciaRecebedor);
-                        try {
-                            contaLogada.transferenciaDinheiro(contaRecebedor, valor);
-                        } catch (RuntimeException error) {
-                            System.err.println(error);
-                        }
-                        break;
-                    case 4:
-                        System.out.println("SEU SALDO É: "+contaLogada.getSaldoConta());
-                    default:
-                        contaLogada = null;
+                    int numeroContaRecebedor = ValidarEntradas.retornarInt("Digite o número da conta do recebedor: ");
+                    int numeroAgenciaRecebedor = ValidarEntradas.retornarInt("Digite o número da agência do recebedor: ");
+
+                    float valor = ValidarEntradas.retornarFloat("Digite o valor da transferência: ");
+                    Conta contaRecebedor = banco.buscarConta(numeroContaRecebedor, numeroAgenciaRecebedor);
+                    try {
+                        contaLogada.transferenciaDinheiro(contaRecebedor, valor);
+                    } catch (RuntimeException error) {
+                        System.err.println(error);
+                    }
+
+                } else if (opcao == 4) {
+
+                    System.out.println("SEU SALDO É: " + contaLogada.getSaldoConta());
+
+                } else {
+
+                    contaLogada = null;
+
                 }
+
             } else {
                 Menu.exibirMenuInicial();
                 int opcao = ValidarEntradas.retornarInt("Digite a opção que você deseja: ");
-                switch (opcao) {
-                    case 1:
-                        int numeroConta = ValidarEntradas.retornarInt("Digite o número da sua conta: ");
-                        int numeroAgencia = ValidarEntradas.retornarInt("Digite o número da sua agência: ");
 
-                        contaLogada = banco.buscarConta(numeroConta, numeroAgencia);
-                        if (contaLogada == null) {
-                            System.err.println("CONTA NÃO ENCONTRADA!");
-                        }
-                        break;
-                    case 2:
-                        String nome = ValidarEntradas.retornarString("Digite o seu nome: ");
-                        int idade = ValidarEntradas.retornarInt("Digite sua idade: ");
-                        String sexo = ValidarEntradas.retornarString("Digite seu sexo: ");
-                        String cpf = ValidarEntradas.retornarString("Digite seu CPF: ");
-                        String rg = ValidarEntradas.retornarString("Digite seu RG: ");
+                if (opcao == 1) {
+                    int numeroConta = ValidarEntradas.retornarInt("Digite o número da sua conta: ");
+                    int numeroAgencia = ValidarEntradas.retornarInt("Digite o número da sua agência: ");
 
-                        banco.criarConta(nome, idade, sexo, cpf, rg);
-                        break;
-                    default:
-                        break;
+                    contaLogada = banco.buscarConta(numeroConta, numeroAgencia);
+                    if (contaLogada == null) {
+                        System.err.println("CONTA NÃO ENCONTRADA!");
+                    }
+                } else if (opcao == 2) {
+                    String nome = ValidarEntradas.retornarString("Digite o seu nome: ");
+                    int idade = ValidarEntradas.retornarInt("Digite sua idade: ");
+                    String sexo = ValidarEntradas.retornarString("Digite seu sexo: ");
+                    String cpf = ValidarEntradas.retornarString("Digite seu CPF: ");
+                    String rg = ValidarEntradas.retornarString("Digite seu RG: ");
+
+                    banco.criarConta(nome, idade, sexo, cpf, rg);
+                } else {
+                    break;
                 }
             }
         }
